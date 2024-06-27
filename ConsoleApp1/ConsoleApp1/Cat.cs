bffbbfd239fe3feae78f68f580f9bf130a55575b
Zaikin-Kirill <zaikin.ssau@yandex.ru>
2024-06-27 19:51:26 +0400

    add class Cat

---
+++ ConsoleApp1/ConsoleApp1/Cat.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1;
+
+public class Cat : Animals
+{
+    public void move()
+    {
+        Console.WriteLine("Идет");
+    }
+
+    public void eat()
+    {
+        Console.WriteLine("Кушает");
+    }
+
+    public void say()
+    {
+        Console.WriteLine("Мяу");
+    }
+}
